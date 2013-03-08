aedb89dd9ca49c1638a119ff965692445adf8eca
mr.savec79@gmail.com <mr.savec79@gmail.com@8d9b2862-0e51-14dc-5763-6c158edfd7ec>
2013-03-08 18:43:55 +0000

    dummy logger added

--- trunk/version.h
+++ trunk/version.h
@@ -10,8 +10,8 @@
 
 #include "TCPIP Stack/TCPIP.h"
 
-#define SVN_REVISION	"10"
-#define SVN_DATETIME	"2013/03/08 23:41:31"
+#define SVN_REVISION	"11"
+#define SVN_DATETIME	"2013/03/09 00:28:47"
 #define SVN_URL			"https://taxa.googlecode.com/svn/trunk"
 
 rom char * buildlabel = SVN_URL " " SVN_DATETIME;
